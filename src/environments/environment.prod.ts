fc6afaf518d43f6d93a1c53ff31173ac02412029
adam.zmijewski <adam.zmijewski@bluesoft.com>
2021-01-07 19:22:36 +0100

    Prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://51.83.132.94:8080/api/',
+  photoUrl: 'http://51.83.132.94:8080/api/images/',
+  keycloakUrl: 'http://51.83.132.94:8000/auth/realms/serwis-aukcyjny/protocol/openid-connect/token',
+  ClientId : 'webapp',
+  ClientSecret: 'ee35e12e-8242-4218-97a8-7c51b85f2758',
 };
